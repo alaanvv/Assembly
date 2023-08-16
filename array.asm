c656e4ab200c13ae6f4e4372f475b3ecc03ad8f4
Alan Vale <valealan84@gmail.com>
2023-08-16 19:40:36 -0300

    Update array.asm
    now i dont need to loop manually, but need to set length manually

--- array.asm
+++ array.asm
@@ -1,48 +1,36 @@
 .data
+listlen:
+	.word 9
 list:
 	.word 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 .text
 .global _start
 _start:
-	@ r0 Current offset
-	@ r1 Current value
-	@ r2 Acumulator
+	@ r0 Current memory offset
+	@ r1 Remaining items (for loop)
+	@ r2 Current value
+	@ r3 Acumulator
+	@ r4 0 (for comparing purposes)
 	
 	LDR r0, =list @ Start r0 as the first element
-	
-	@ Loop like a boss B)
+	@ I don't know if i should do that, but, basically the listlen is always 
+	@ stored before the list, so i just use the list pointer substracting 4
+	LDR r1, [r0, #-4]
 	
 	@ Pre-increment
 	@ [addr, #offset]! Increase the addr pointer value by offset and read it
 	@ Post-increment
 	@ [addr], #offset Read the value the increase it
-	LDR r1, [r0], #4  @ Read current index, and increment 4 bits in it
-	ADD r2, r1	  @ Sum current value to r2
 	
-	LDR r1, [r0], #4
-	ADD r2, r1
+read:
+	LDR r2, [r0], #4
+	ADD r3, r2 @ Add the value to acumulator
+	SUB r1, #1 @ Decrease remaining items
 	
-	LDR r1, [r0], #4
-	ADD r2, r1
+	CMP r1, r4
 	
-	LDR r1, [r0], #4
-	ADD r2, r1
-	
-	LDR r1, [r0], #4
-	ADD r2, r1
-	
-	LDR r1, [r0], #4
-	ADD r2, r1
-	
-	LDR r1, [r0], #4
-	ADD r2, r1
-	
-	LDR r1, [r0], #4
-	ADD r2, r1
-	
-	LDR r1, [r0], #4
-	ADD r2, r1
+	BGT read
 	
 	MOV r7, #1
 	SWI 0
