878e9d84f231233177acdc93a2a163f87b1ca2b6
Alan Vale <valealan84@gmail.com>
2023-08-16 21:41:20 -0300

    Update circle-area.asm
    removed bad comments

--- circle-area.asm
+++ circle-area.asm
@@ -1,14 +1,13 @@
 .text
 .global _start
 _start:
-	@ Radius of the circle
-	MOV r0, #3
-	@ PI (idk float pointing yet)
-	MOV r1, #3
+	MOV r0, #5 @ Radius of the circle
+	MOV r1, #3 @ PI
 
-	@ Circle area is equal PI*R^2	
-	MUL r0, r0 @ Square radius
-	MUL r0, r1 @ Multiply by PI
+	@ Circle area = PI*R^2	
+	MUL r0, r0
+	MUL r0, r1
 	
+end:
 	MOV r7, #1
 	SWI 0
